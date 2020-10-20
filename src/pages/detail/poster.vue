8ade1043afceedaf3ef85c5451aa50dd27b0c9ac
Calvin1121 <461858277@qq.com>
2020-10-20 20:38:52 +0800

    ..

--- src/pages/detail/poster.vue
+++ src/pages/detail/poster.vue
@@ -11,33 +11,38 @@
 </template>
 <script>
 const img = '../../static/img.png'
+const code = '../../static/avatar.png'
 export default {
     name: 'poster',
-    created(){
-    	this.draw()
+    created() {
+        this.draw()
     },
-    methods:{
-    	draw(){
-    		let ctx = wx.createCanvasContext("poster"); 
-    		ctx.setFillStyle ("#ffffff")
-    		ctx.rect(0, 0, uni.upx2px(664), uni.upx2px(1030))
-    		ctx.fill()
-    		ctx.drawImage(img, 0, 0, uni.upx2px(664), uni.upx2px(440));
-    		ctx.save();
-    		ctx.beginPath();
-    		ctx.fillStyle = "#342369";
-    		ctx.font = `normal bold ${uni.upx2px(36)}px sans-serif`;
-    		ctx.fillText('c64', uni.upx2px(28), uni.upx2px(510));
-    		ctx.font = `normal regular ${uni.upx2px(30)}px sans-serif`;
-    		ctx.fillText('crp keycaps', uni.upx2px(28), uni.upx2px(560));
-    		ctx.font = `normal regular ${uni.upx2px(28)}px sans-serif`;
-    		ctx.fillText('MDA高度键帽，由MELGEEK制造', uni.upx2px(28), uni.upx2px(610));
-    		ctx.font = `normal bold ${uni.upx2px(40)}px sans-serif`;
-    		ctx.fillText('长按识别小程序', uni.upx2px(36), uni.upx2px(850));
-    		ctx.font = `normal regular ${uni.upx2px(28)}px sans-serif`;
-    		ctx.fillText('好货要和朋友一起分享', uni.upx2px(36), uni.upx2px(910));
-    		ctx.draw()
-    	}
+    methods: {
+        draw() {
+            let ctx = wx.createCanvasContext("poster");
+            ctx.setFillStyle("#ffffff")
+            ctx.rect(0, 0, uni.upx2px(664), uni.upx2px(1030))
+            ctx.fill()
+            ctx.drawImage(img, 0, 0, uni.upx2px(664), uni.upx2px(440));
+            ctx.save();
+            ctx.beginPath();
+            ctx.fillStyle = "#342369";
+            ctx.font = `normal bold ${uni.upx2px(36)}px sans-serif`;
+            ctx.fillText('c64', uni.upx2px(28), uni.upx2px(510));
+            ctx.font = `normal regular ${uni.upx2px(30)}px sans-serif`;
+            ctx.fillText('crp keycaps', uni.upx2px(28), uni.upx2px(560));
+            ctx.font = `normal regular ${uni.upx2px(28)}px sans-serif`;
+            ctx.fillText('MDA高度键帽，由MELGEEK制造', uni.upx2px(28), uni.upx2px(610));
+            ctx.font = `normal bold ${uni.upx2px(40)}px sans-serif`;
+            ctx.fillText('长按识别小程序', uni.upx2px(36), uni.upx2px(860));
+            ctx.font = `normal regular ${uni.upx2px(28)}px sans-serif`;
+            ctx.fillText('好货要和朋友一起分享', uni.upx2px(36), uni.upx2px(920));
+            ctx.setStrokeStyle('#000000');
+            ctx.setLineWidth(uni.upx2px(2));
+            ctx.arc(uni.upx2px(516), uni.upx2px(872), uni.upx2px(96), 0, Math.PI * 2);
+            ctx.stroke();
+            ctx.draw()
+        }
     }
 }
 </script>
